d2a24c5f5bdb42ded00fe30cb4f52372f4c056dc
Jip Fr <jipfrijlink@gmail.com>
2023-06-09 04:17:44 +0200

    first commit

---
+++ src/lib/stuff.svelte
@@ -0,0 +1,161 @@
+<script>
+	import book from '../book.json';
+
+	const weeks = [
+		range(1, 7),
+		range(8, 15),
+		range(16, 22),
+		range(23, 30),
+		range(31, 37),
+		range(38, 43),
+		range(44, 48)
+	];
+
+	const startDate = '14 may 2023 00:00';
+	const now = new Date();
+
+	const weeksWithInfo = weeks.map((week, i) => {
+		let d = new Date(startDate);
+		d.setDate(d.getDate() + 7 * i);
+		const weekAfter = new Date(d.getTime());
+		weekAfter.setDate(weekAfter.getDate() + 7);
+
+		// (10-5)/(15-5)
+		const progressInWeekBase = (now.getTime() - d.getTime()) / (weekAfter.getTime() - d.getTime());
+
+		const progressInWeek = Math.max(0, Math.min(1, progressInWeekBase));
+
+		return {
+			weekNo: i + 1,
+			startDate: d,
+			chapters: week.map((chNo, i2) => {
+				const percentageInWeek = (1 / week.length) * i2;
+				return {
+					chNo,
+					percentageInWeek,
+					progressInWeek,
+					weekNo: i + 1
+				};
+			}),
+			progressInWeek
+		};
+	});
+
+	const allChapters = weeksWithInfo.flatMap((w) => w.chapters);
+	allChapters.map((ch, i) => {
+		// References babyy!
+		let value = ch.progressInWeek;
+		let chapterStart = ch.percentageInWeek;
+		let chapterEnd = allChapters[i + 1]?.percentageInWeek || 1;
+
+		// if (chapterEnd === 1) {
+		// 	// Final chapter in week, use different calculation
+		// 	const currentWeek = weeksWithInfo.find((week) => week.weekNo === ch.weekNo);
+		// 	const thisWeekStart = currentWeek.startDate;
+		// 	const nextWeekStart =
+		// 		weeksWithInfo[weeksWithInfo.indexOf(currentWeek) + 1]?.startDate ?? new Date('1 jan 3000');
+		// 	const weekLength = nextWeekStart.getTime() - thisWeekStart.getTime();
+
+		// 	const percentageIntoWeek =
+		// 		(1 / currentWeek.chapters.length) * (currentWeek.chapters.length - 1);
+
+		// 	const chDate = new Date(thisWeekStart.getTime() + weekLength * percentageIntoWeek);
+
+		// 	chapterStart = chDate.getTime();
+		// 	chapterEnd = nextWeekStart.getTime();
+		// 	value = now.getTime();
+
+		// 	// console.log(new Date(chapterStart));
+		// 	// console.log(new Date(chapterEnd));
+		// 	// console.log(new Date(now));
+		// }
+
+		// I can't do math
+		let percentageUntilNext = (value - chapterStart) / (chapterEnd - chapterStart);
+
+		ch.percentageUntilNext = Math.min(1, Math.max(0, percentageUntilNext));
+		console.log(ch.chNo, '—', value, chapterStart, chapterEnd, ch.percentageUntilNext);
+
+		ch.hasRead = ch.percentageUntilNext > 0;
+	});
+
+	function range(a, b) {
+		return Array(b - a + 1)
+			.fill(0)
+			.map((_, i) => i + a);
+	}
+</script>
+
+{#each weeksWithInfo as week}
+	<div class="week">
+		<h2>Week {week.weekNo}</h2>
+		{#each week.chapters as { chNo, hasRead, percentageUntilNext }}
+			{@const chapter = book.find((t) => t.ch === chNo)}
+			<div class="chapter">
+				<div class="left">
+					<div class="ball" has-read={hasRead} />
+					<div class="line" style={`--height: ${percentageUntilNext * 100}%`} />
+				</div>
+				<div>
+					<p>Chapter {chNo}</p>
+					<p class="quote">
+						{@html chapter.quote
+							.split('\n')
+							.map((t) => (t.startsWith('-') ? `<strong>${t}</strong>` : t))
+							.join('\n')}
+					</p>
+				</div>
+			</div>
+		{/each}
+	</div>
+{/each}
+
+<style>
+	.chapter {
+		display: grid;
+		grid-template-columns: 16px 1fr;
+		grid-gap: 20px;
+		margin: 30px 0;
+		--gray: rgba(0, 0, 0, 0.2);
+		--green: rgb(54, 137, 54);
+	}
+
+	.ball {
+		width: 100%;
+		aspect-ratio: 1/1;
+		background: var(--gray);
+		border-radius: 50%;
+		position: relative;
+		z-index: 10;
+	}
+	.ball[has-read='true'] {
+		background: var(--green);
+	}
+
+	.line {
+		width: 3px;
+		margin-top: -8px;
+		height: calc(100% + 8px + 20px);
+		margin-left: calc(16px / 2 - 3px / 2);
+		z-index: -1;
+
+		background: linear-gradient(to bottom, var(--green) var(--height), var(--gray) var(--height));
+	}
+
+	.chapter div p:first-child {
+		margin-top: 0;
+	}
+	.chapter div p:last-child {
+		margin-bottom: 0;
+	}
+
+	.quote {
+		white-space: pre-wrap;
+	}
+
+	@media (prefers-color-scheme: dark) {
+		.chapter {
+			--gray: #474747;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+	import Stuff from '../lib/stuff.svelte';
+</script>
+
+<div class="container">
+	<h1>Dune re-read progress indicator</h1>
+	<p>
+		Not sure what this is? Join the Dune discord. <a href="https://discord.gg/dune" target="_blank">
+			https://discord.gg/dune
+		</a>
+	</p>
+	<p>Either way, the chapters must flow.</p>
+
+	<hr />
+
+	<div>
+		<p><strong>Book One</strong></p>
+		<p>week 1: Chapter 1-7 "A beginning..."</p>
+		<p>week 2: Chapter 8-15 "Yueh! Yueh! Yueh!"</p>
+		<p>week 3: Chapter 16-22 "Greatness is a transitory experience"</p>
+		<p><strong>Book Two: Muad'Dib</strong></p>
+		<p>week 4: Chapter 23-30 "When my father, the Padishah Emperor..."</p>
+		<p>week 5: Chapter 31-37 "Prophecy and prescience"</p>
+		<p><strong>Book Three: The Prophet</strong></p>
+		<p>week 6: Chapter 38-43 "No woman, no man, no child..."</p>
+		<p>week 7: Chapter 44-48 "How often it is"</p>
+	</div>
+
+	<hr />
+
+	<Stuff />
+</div>
+
+<style>
+	.container {
+		width: 90%;
+		max-width: 800px;
+		margin: 90px auto;
+	}
+	hr {
+		margin: 40px 0;
+	}
+</style>
